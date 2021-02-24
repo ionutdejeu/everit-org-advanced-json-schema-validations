3b21955d3e7d9696d5e19da9c9f2c2714a0ce95c
ionutdejeu <ionut.dejeu@gmail.com>
2021-02-24 10:11:05 +0200

    Some examples of json schema validations

--- src/test/java/JSONSchemaTest.java
+++ src/test/java/JSONSchemaTest.java
@@ -4,6 +4,15 @@ import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.junit.Test;
+import sun.misc.IOUtils;
+
+import javax.print.DocFlavor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.sql.Struct;
 
 public class JSONSchemaTest {
     @Test
@@ -22,4 +31,82 @@ public class JSONSchemaTest {
             String msg= e.getMessage();
         }
     }
+
+    @Test
+    public void any_off_json_schema_test() throws ValidationException {
+        JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/any_of_json_schema.json")));
+        JSONObject jsonSubject = new JSONObject(
+                new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/obj_invalid_any_of.json")));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+        try {
+            schema.validate(jsonSubject);
+        }catch (ValidationException e){
+            Schema s= e.getViolatedSchema();
+
+            String msg= e.getMessage();
+        }
+    }
+
+    @Test
+    public void ifthen_test() throws ValidationException, URISyntaxException, IOException {
+        JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/ifthen/schema.json")));
+        java.nio.file.Path i= java.nio.file.Paths.get(getClass().getClassLoader().getResource("ifthen/obj.json").toURI());
+
+        String theString = new String(Files.readAllBytes(i));
+        JSONObject jsonSubject = new JSONObject(
+                new JSONTokener(theString.toLowerCase()));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+        try {
+            schema.validate(jsonSubject);
+        }catch (ValidationException e){
+            Schema s= e.getViolatedSchema();
+
+            String msg= e.getMessage();
+        }
+    }
+
+    @Test
+    public void regex_attribute_test() throws ValidationException, URISyntaxException, IOException {
+        JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/obj_properties_regex/schema.json")));
+        java.nio.file.Path i= java.nio.file.Paths.get(getClass().getClassLoader().getResource("obj_properties_regex/obj.json").toURI());
+
+        String theString = new String(Files.readAllBytes(i));
+        JSONObject jsonSubject = new JSONObject(
+                new JSONTokener(theString.toLowerCase()));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+        try {
+            schema.validate(jsonSubject);
+        }catch (ValidationException e){
+            Schema s= e.getViolatedSchema();
+
+            String msg= e.getMessage();
+        }
+    }
+
+    @Test
+    public void format_attribute_test() throws ValidationException, URISyntaxException, IOException {
+        JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/format/schema.json")));
+        java.nio.file.Path i= java.nio.file.Paths.get(getClass().getClassLoader().getResource("format/obj.json").toURI());
+
+        String theString = new String(Files.readAllBytes(i));
+        JSONObject jsonSubject = new JSONObject(
+                new JSONTokener(theString.toLowerCase()));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+        try {
+            schema.validate(jsonSubject);
+        }catch (ValidationException e){
+            Schema s= e.getViolatedSchema();
+
+            String msg= e.getMessage();
+        }
+    }
+
 }
