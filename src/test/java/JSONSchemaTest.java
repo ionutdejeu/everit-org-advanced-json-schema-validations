f5c0290923035832ef650b9e6c419cf9a1f6a604
ionutdejeu <ionut.dejeu@gmail.com>
2021-02-27 21:33:34 +0200

    Added extra sample validations

--- src/test/java/JSONSchemaTest.java
+++ src/test/java/JSONSchemaTest.java
@@ -109,4 +109,44 @@ public class JSONSchemaTest {
         }
     }
 
+    @Test
+    public void attribute_type_validation() throws ValidationException, URISyntaxException, IOException {
+        JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/type_validations/schema.json")));
+        java.nio.file.Path i= java.nio.file.Paths.get(getClass().getClassLoader().getResource("type_validations/obj.json").toURI());
+
+        String theString = new String(Files.readAllBytes(i));
+        JSONObject jsonSubject = new JSONObject(
+                new JSONTokener(theString.toLowerCase()));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+        try {
+            schema.validate(jsonSubject);
+        }catch (ValidationException e){
+            Schema s= e.getViolatedSchema();
+
+            String msg= e.getMessage();
+        }
+    }
+
+    @Test
+    public void attribute_enumeration_validation() throws ValidationException, URISyntaxException, IOException {
+        JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/type_enum/schema.json")));
+        java.nio.file.Path i= java.nio.file.Paths.get(getClass().getClassLoader().getResource("type_enum/obj.json").toURI());
+
+        String theString = new String(Files.readAllBytes(i));
+        JSONObject jsonSubject = new JSONObject(
+                new JSONTokener(theString.toLowerCase()));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+        try {
+            schema.validate(jsonSubject);
+        }catch (ValidationException e){
+            Schema s= e.getViolatedSchema();
+
+            String msg= e.getMessage();
+        }
+    }
+
 }
